e8308a3cdd844a55a2e7a104db8f209365a1ae47
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2022-06-30 23:28:25 +0900

    タイトル

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
 
   return (
     <>
-      <div className="border-b">
+      <div>
         <nav className="bg-white dark:bg-gray-800 ">
           <div className="max-w-7xl mx-auto px-8">
             <div className="flex items-center justify-between h-16">
@@ -60,7 +60,7 @@ export const Header = () => {
               </div>
             </div>
           </div>
-          <div className={`transition duration-500 ease-in-out ${!isMenuOpened && 'hidden'}`}>
+          <div className={`${!isMenuOpened && 'hidden'}`}>
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <a
                 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium"
--- src/components/page/Top/Top.tsx
+++ src/components/page/Top/Top.tsx
@@ -2,6 +2,7 @@ import { Card } from '@/components/page/Top/Card';
 export const Top = () => {
   return (
     <>
+      <h2 className="m-14 text-3xl font-bold text-center">大会一覧</h2>
       <div className="flex flex-wrap m-10">
         <Card />
         <Card />
