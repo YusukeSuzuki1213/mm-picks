54f4853b73647cfb0b066a719fc71a99a61597d8
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2022-07-03 02:11:29 +0900

    added liner graph, image border

--- src/components/page/Event/Match/Match.tsx
+++ src/components/page/Event/Match/Match.tsx
@@ -1,27 +1,36 @@
 export const Match = () => {
   return (
     <>
-      <div className="flex items-center gap-8">
-        <div className="flex flex-col items-center">
-          <div
-            className="h-20 w-20 rounded-full bg-center"
-            style={{
-              backgroundImage: "url('https://onl.tw/c8emWAL')",
-              backgroundSize: '7rem',
-            }}
-          />
-          <button>WIN</button>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center gap-8">
+          <div className="flex flex-col items-center">
+            <div
+              className="h-20 w-20 rounded-full bg-center border-2 border-white"
+              style={{
+                backgroundImage: "url('https://onl.tw/c8emWAL')",
+                backgroundSize: '7rem',
+                outline: 'solid 2px #F00',
+              }}
+            />
+            <p>萩原京平</p>
+          </div>
+          <div>VS</div>
+          <div className="flex flex-col items-center">
+            <div
+              className="h-20 w-20 rounded-full bg-center border-2 border-white"
+              style={{
+                backgroundImage: "url('https://onl.tw/c8emWAL')",
+                backgroundSize: '7rem',
+                outline: 'solid 2px #00F',
+              }}
+            />
+            <p>萩原京平</p>
+          </div>
         </div>
-        <div>VS</div>
-        <div className="flex flex-col items-center">
-          <div
-            className="h-20 w-20 rounded-full bg-center"
-            style={{
-              backgroundImage: "url('https://onl.tw/c8emWAL')",
-              backgroundSize: '7rem',
-            }}
-          />
-          <button>WIN</button>
+
+        <div className="flex w-full h-5">
+          <div className="bg-red-500 w-[60%]" />
+          <div className="bg-blue-500 w-[40%]" />
         </div>
       </div>
     </>
